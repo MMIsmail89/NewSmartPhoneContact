3d902bb5ce50368a0003d57adc2bf605cfcb4aad
Magdi <86538068+MMIsmail89@users.noreply.github.com>
2023-03-05 21:08:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/addnewmobilecontact/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.addnewmobilecontact;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.addnewmobilecontact.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    //
+    ActivityMainBinding binding;
+    public final int Contact_REQ_CODE = 1;
+    String newUserName = "", newUserPhone = "",
+            newUserWebLink = "", newUserPostalAddress ="", newUserMood = "";
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //
+
+        binding.mainBtnCreateNewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        createContactActivity.class);
+                startActivityForResult(intent, Contact_REQ_CODE);
+            }
+        });
+        //
+        binding.mainIvPhone.setVisibility(View.GONE);
+        binding.mainIvWebPage.setVisibility(View.GONE);
+        binding.mainIvLoction.setVisibility(View.GONE);
+        binding.mainIvUserMood.setVisibility(View.GONE);
+        binding.mainTvNewContactuserName.setVisibility(View.GONE);
+        //
+        binding.mainIvPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:" + newUserPhone));
+                startActivity(intent);
+            }
+        });
+
+        binding.mainIvWebPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://" + newUserWebLink));
+                startActivity(intent);
+            }
+        });
+
+        binding.mainIvLoction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo:0,0?q=" + newUserPostalAddress));
+                startActivity(intent);
+            }
+        });
+
+        //
+    }
+    //
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //
+        if(requestCode == Contact_REQ_CODE){
+            if (resultCode == createContactActivity.Contact_Done_RES_CODE){
+                binding.mainIvPhone.setVisibility(View.VISIBLE);
+                binding.mainIvWebPage.setVisibility(View.VISIBLE);
+                binding.mainIvLoction.setVisibility(View.VISIBLE);
+                binding.mainIvUserMood.setVisibility(View.VISIBLE);
+                //
+                newUserName = data.getStringExtra(createContactActivity.Name_Key);
+                binding.mainTvNewContactuserName.setText(getString(R.string.new_user_name) + " " + newUserName);
+                binding.mainTvNewContactuserName.setVisibility(View.VISIBLE);
+
+                newUserPhone = data.getStringExtra(createContactActivity.Phone_Key);
+
+                newUserPostalAddress = data.getStringExtra(createContactActivity.PostalAddress_Key);
+
+                newUserWebLink = data.getStringExtra(createContactActivity.Web_Key);
+
+                newUserMood = data.getStringExtra(createContactActivity.MOOD_Key);
+                //
+                if (newUserMood.equals(createContactActivity.happy)){
+                    binding.mainIvUserMood.setImageResource(R.drawable.satisfied);
+                } else if (newUserMood.equals(createContactActivity.okay)) {
+                    binding.mainIvUserMood.setImageResource(R.drawable.neutral);
+                }else { // newUserMood.equals(createContactActivity.sad
+                    binding.mainIvUserMood.setImageResource(R.drawable.very_dissatisfied);
+                }
+
+            }
+            else {
+                Toast.makeText(MainActivity.this, getString(R.string.no_data_passed_through),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater_mainMenu = getMenuInflater();
+
+        inflater_mainMenu.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId())
+        {
+            case R.id.main_menu_author:
+                Intent intent = new Intent(MainActivity.this,
+                        authorMagdi.class);
+                startActivity(intent);
+
+                return true;
+
+        } // switch (item.getItemId())
+        return false; // if the switch is not any true
+    }
+    //
+}
---
+++ app/src/main/java/com/example/addnewmobilecontact/createContactActivity.java
@@ -0,0 +1,116 @@
+package com.example.addnewmobilecontact;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.addnewmobilecontact.databinding.ActivityCreateContactBinding;
+
+public class createContactActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //
+    ActivityCreateContactBinding binding;
+    public static final String Name_Key = "userName";
+    public static final String Phone_Key = "userPhone";
+    public static final String Web_Key = "userWebAddress";
+    public static final String PostalAddress_Key = "userPostalAddress";
+    public static final String MOOD_Key = "userMOOD";
+    public static final String happy = "happy";
+    public static final String okay = "okay";
+    public static final String sad = "sad";
+    public static final int Contact_Done_RES_CODE = 1;
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCreateContactBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        //
+        binding.contactIvHappy.setOnClickListener(this);
+        //
+        binding.contactIvOkay.setOnClickListener(this);
+        //
+        binding.contactIvSad.setOnClickListener(this);
+        //
+    }
+    //
+
+    //
+
+    @Override
+    public void onClick(View v) {
+        //
+        String userName = binding.contactEdUserName.getText().toString();
+        String userPhone = binding.contactEdUserPhone.getText().toString();
+        String userWebAddress = binding.contactEdUserWebPage.getText().toString();
+        String userPostalAddress = binding.contactEdUserPostalAddress.getText().toString();
+        //
+        if (userName.isEmpty() ||
+                userPhone.isEmpty() ||
+                userWebAddress.isEmpty() ||
+                userPostalAddress.isEmpty()){
+            Toast.makeText(createContactActivity.this, R.string.fill_all_fields_valid_data,
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent();
+
+            intent.putExtra( Name_Key, userName.trim());
+            intent.putExtra( Phone_Key, userPhone.trim());
+            intent.putExtra( Web_Key, userWebAddress.trim());
+            intent.putExtra( PostalAddress_Key, userPostalAddress.trim());
+
+            if (v.getId() == R.id.contact_iv_happy){
+                intent.putExtra( MOOD_Key, happy);
+            } else if (v.getId() == R.id.contact_iv_okay) {
+                intent.putExtra( MOOD_Key, okay);
+            } else {// (view.getId() == R.id.contact_iv_sad)
+                intent.putExtra( MOOD_Key, sad);
+            }
+            //
+            setResult(Contact_Done_RES_CODE, intent);
+            createContactActivity.this.finish();
+
+
+        }
+
+    }
+
+    //
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater_mainMenu = getMenuInflater();
+
+        inflater_mainMenu.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId())
+        {
+            case R.id.main_menu_author:
+                Intent intent = new Intent(createContactActivity.this,
+                        authorMagdi.class);
+                startActivity(intent);
+
+                return true;
+
+        } // switch (item.getItemId())
+        return false; // if the switch is not any true
+    }
+    //
+}
